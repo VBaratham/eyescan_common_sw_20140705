257c2b457d229a987a0004306d3331c159dfb445
Daniel Boline <daniel.boline@stonybrook.edu>
2014-07-24 09:29:29 -0400

    stuff

--- tcl_scripts/test_socket.tcl
+++ tcl_scripts/test_socket.tcl
@@ -2,7 +2,7 @@ for { set N 0 } { $N < 8 } { incr N } {
 set chan [socket 192.168.1.99 7]
 puts $chan "dbgeyescan $N"
 flush $chan
-for { set i 0 } { $i < 180 } { incr i } {
+for { set i 0 } { $i < 190 } { incr i } {
 	puts [gets $chan]
 }
 close $chan
@@ -15,10 +15,10 @@ set number [gets $chan]
 puts $number
 close $chan
 
-gets stdin
+#gets stdin
 
-set chan [socket 192.168.1.99 7]
-puts $chan "esread 0 $number"
-flush $chan
-puts [gets $chan]
-close $chan
+#set chan [socket 192.168.1.99 7]
+#puts $chan "esread 0 $number"
+#flush $chan
+#puts [gets $chan]
+#close $chan
