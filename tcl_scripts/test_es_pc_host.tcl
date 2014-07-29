720619cd80adc6dc62e38a2fab786cfb509c2839
Daniel Boline <daniel.boline@stonybrook.edu>
2014-07-29 13:37:15 -0400

    small changes

--- tcl_scripts/test_es_pc_host.tcl
+++ tcl_scripts/test_es_pc_host.tcl
@@ -19,7 +19,7 @@ proc run_test {} {
         # set horz_step($i) 255
         
         # vert_step: Vertical sweep step size. Min value is 1. Max value is 127.
-        # set vert_step($i) 8
+        #set vert_step($i) 8
         # use this setting to generate 1-d bathtub plots
         set vert_step($i) 127
         
@@ -27,8 +27,8 @@ proc run_test {} {
         set max_prescale($i) 8
         
         # data_width: Parallel data width interface. Valid values are 16, 20, 32, 40.
-        set data_width($i) 20
-        #set data_width($i) 40
+        #set data_width($i) 20
+        set data_width($i) 40
 
         # lpm_mode: Set to 1 for LPM mode. Set to 0 for DFE mode.
         set lpm_mode($i) 0
@@ -37,7 +37,7 @@ proc run_test {} {
         #full=32,half=64,qrtr=128,octal=256,hex=512
         #set rate($i) 32			
         #set rate($i) 64
-        set rate($i) 128
+        #set rate($i) 128
         set rate($i) 256
         #set rate($i) 512
         
